2c464e74b02e3786eabbc0ad24e53563912f876e
Uranus_21 <urjadoshi12@gmail.com>
2026-03-24 21:00:55 +0530

    Update drive_auth.py

--- Software/Automated_Visitor_Pass_Generator/src/drive/drive_auth.py
+++ Software/Automated_Visitor_Pass_Generator/src/drive/drive_auth.py
@@ -1,1 +1,6 @@
+from googleapiclient.discovery import build
+import google.auth
 
+def authenticate_drive():
+    credentials, _ = google.auth.default()
+    return build('drive', 'v3', credentials=credentials)
