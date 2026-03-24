94d93f654507682f97b3671e57156051b34d31e6
Uranus_21 <urjadoshi12@gmail.com>
2026-03-24 21:00:20 +0530

    Update fetch_data.py

--- Software/Automated_Visitor_Pass_Generator/src/sheets/fetch_data.py
+++ Software/Automated_Visitor_Pass_Generator/src/sheets/fetch_data.py
@@ -1,1 +1,17 @@
+from gspread_dataframe import get_as_dataframe
 
+def fetch_participants(gc, spreadsheet_url, worksheet_name):
+    sheet = gc.open_by_url(spreadsheet_url)
+    worksheet = sheet.worksheet(worksheet_name)
+
+    df = get_as_dataframe(worksheet)
+    df = df.dropna(how='all')
+
+    df = df.rename(columns={
+        'Upload Passport Size Photo': 'Photo URL'
+    })
+
+    if 'Pass Generated' not in df.columns:
+        df['Pass Generated'] = ''
+
+    return df, worksheet
