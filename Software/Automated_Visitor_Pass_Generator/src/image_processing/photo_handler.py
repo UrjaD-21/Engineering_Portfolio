da49faa0b4489d6d9f616b81d21861f2f141ea6c
Uranus_21 <urjadoshi12@gmail.com>
2026-03-24 21:02:54 +0530

    Update photo_handler.py

--- Software/Automated_Visitor_Pass_Generator/src/image_processing/photo_handler.py
+++ Software/Automated_Visitor_Pass_Generator/src/image_processing/photo_handler.py
@@ -1,1 +1,7 @@
+from PIL import Image
 
+def process_photo(photo_path, size, radius):
+    img = Image.open(photo_path).convert("RGBA")
+    img = img.resize(size)
+    # apply rounded corners
+    return img
